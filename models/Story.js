9ea18d9add5b6202ccb18c200c128ba473cebaa0
Andrew Bao <bao.andrew@gmail.com>
2018-08-29 19:02:24 -0700

    updated story model

--- models/Story.js
+++ models/Story.js
@@ -3,13 +3,21 @@ const mongoose = require('mongoose')
 likeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
+platformSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    platformVote: {type: String, required: true}
+})
+genreSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    genreVote: {type: String, required: true}
+})
   
 const storySchema = new mongoose.Schema({
     title: {type: String, required: true, unique: true},
     description: {type: String, required: true}, 
     body: {type: String, required: true},
-    platform: [{type: String, required: true}],
-    genre: [{type: String, required: true}],
+    platform: [platformSchema],
+    genre: [genreSchema],
     likes: [likeSchema], 
     _by: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
